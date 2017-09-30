a2500d72bd65fb18ea30a54cafc82b071017af55
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 03:33:15 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   # your code here
   squared_numbers = []
-  array.each do |number|
+  array.collect do |number|
     number *= number
-    squared_numbers << number
   end
-  squared_numbers
 end
+
